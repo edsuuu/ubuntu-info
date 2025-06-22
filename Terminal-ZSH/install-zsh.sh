abad4b365e4a6ebc1c0cc9f3283047a2ea9871a4
edsuuu <111332935+edsuuu@users.noreply.github.com>
2025-06-22 19:25:36 -0300

    Update install-zsh.sh

--- Terminal-ZSH/install-zsh.sh
+++ Terminal-ZSH/install-zsh.sh
@@ -13,48 +13,41 @@ spinner() {
     local spinstr='|/-\'
     while kill -0 $pid 2>/dev/null; do
         local temp=${spinstr#?}
-        printf " [%c]  " "$spinstr"
+        printf "\r[%c]  " "$spinstr"
         spinstr=$temp${spinstr%"$temp"}
         sleep $delay
-        printf "\b \b \n"
     done
+    printf "\b\b\b\b"
 }
 
 echo -e "\n${BLUE}✅ Atualizando pacotes...${NC}\n"
+(sudo apt update -y && sudo apt upgrade -y > /dev/null 2>&1) & spinner
 
-(sudo apt update -y && sudo apt upgrade -y) > /dev/null 2>&1 & spinner
+echo -e "${BLUE}⚡ Instalando ZSH... ${NC}\n"
+(sudo apt install zsh -y > /dev/null 2>&1) & spinner
 
-echo -e  "${BLUE}⚡ Instalando ZSH... ${NC}\n"
-
-(sudo apt install zsh -y) > /dev/null 2>&1 & spinner
-
-echo -e  "\n${BLUE}🔧 Alterando shell padrão para ZSH... ${NC}\n"
+echo -e "\n${BLUE}🔧 Alterando shell padrão para ZSH... ${NC}\n"
 chsh -s /bin/zsh
 
-echo -e  "${BLUE}🚀 Instalando Oh My Zsh... ${NC}\n"
-
-(sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null 2>&1) & show_spinner $!
+echo -e "${BLUE}🚀 Instalando Oh My Zsh... ${NC}\n"
+(sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended > /dev/null 2>&1) & spinner
 
 export ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 
 echo -e "${BLUE}✨ Instalando Spaceship Prompt...${NC}\n"
-(git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1 > /dev/null 2>&1) & show_spinner $!
-
+(git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1 > /dev/null 2>&1) & spinner
 
 echo -e "${BLUE}🧲 Instalando Zsh Autosuggestions...${NC}\n"
-
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM}/plugins/zsh-autosuggestions"
 
 echo -e "${BLUE}🖍️ Instalando Zsh Syntax Highlighting...${NC}\n"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM}/plugins/zsh-syntax-highlighting"
 
 echo -e "${BLUE}🛠️ Configurando .zshrc...${NC}\n"
-
 cp ~/.zshrc ~/.zshrc.backup
 
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="duellj"/' ~/.zshrc
 sed -i 's/^plugins=.*/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
-
 grep -q "ZSH_THEME=" ~/.zshrc || echo 'ZSH_THEME="duellj"' >> ~/.zshrc
 
 echo -e "${GREEN}✅ Instalação concluída com sucesso!${NC}\n"
